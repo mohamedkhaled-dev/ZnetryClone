2e93c5ca9181cce3c6c3e1a3eec949187575c405
Mohamed Khaled <mohamedkhaled20031@gmail.com>
2025-06-11 00:44:58 +0300

    Add GSAP and React Icons dependencies; implement Hero and Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-violet-300 font-bold ">
-        Welcome to PixelCrown
-      </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
   );
 };
