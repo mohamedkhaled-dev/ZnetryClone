6735712b3777856e08414b506ea7a8aaadbe17d4
Mohamed Khaled <mohamedkhaled20031@gmail.com>
2025-06-10 23:20:30 +0300

    Initial commit and app setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-violet-300 font-bold ">
+        Welcome to PixelCrown
+      </h1>
+    </main>
+  );
+};
+
+export default App;
